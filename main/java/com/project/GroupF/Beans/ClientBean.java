f94083d7ab58f6af176a26a7562a7e987e06f4d9
Monica <Monica@10.0.0.210>
2022-03-28 23:23:03 -0400

    beans

--- main/java/com/project/GroupF/Beans/ClientBean.java
+++ main/java/com/project/GroupF/Beans/ClientBean.java
@@ -8,5 +8,70 @@ package com.project.GroupF.Beans;
  *
  */
 public class ClientBean {
+	private int client_id;
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private String address;
+	private String city;
+	private String postalCode;
+	private int contactNo;
+	private String email;
+	
+	public int getClient_id() {
+		return client_id;
+	}
+	public void setClient_id(int client_id) {
+		this.client_id = client_id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public int getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(int contactNo) {
+		this.contactNo = contactNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 
 }
