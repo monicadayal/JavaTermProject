646681f9963a9af31b02ae51dba76cb2efa00664
Sophiya <Sophiya@192.168.2.33>
2022-03-30 17:10:20 -0400

    client account data fetch by ID

--- main/java/com/project/GroupF/Beans/AccountBean.java
+++ main/java/com/project/GroupF/Beans/AccountBean.java
@@ -4,9 +4,55 @@
 package com.project.GroupF.Beans;
 
 /**
+ * Date 3/29/2022
  * @author Monica
  *
  */
 public class AccountBean {
-
+	
+	private int account_id;
+	private int type_id;
+	private int client_id;
+	private String account_number;
+	private float balance;
+	private String create_date;
+	
+	public int getAccount_id() {
+		return account_id;
+	}
+	public void setAccount_id(int account_id) {
+		this.account_id = account_id;
+	}
+	public int getType_id() {
+		return type_id;
+	}
+	public void setType_id(int type_id) {
+		this.type_id = type_id;
+	}
+	public int getClient_id() {
+		return client_id;
+	}
+	public void setClient_id(int client_id) {
+		this.client_id = client_id;
+	}
+	public String getAccount_number() {
+		return account_number;
+	}
+	public void setAccount_number(String account_number) {
+		this.account_number = account_number;
+	}
+	public float getBalance() {
+		return balance;
+	}
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+	public String getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(String create_date) {
+		this.create_date = create_date;
+	}
+	
+	
 }
